248b249920e15d5afa5785afa70129258dc715c6
gaurav-kumar17 <gk170106@gmail.com>
2022-08-22 11:57:23 +0530

    another commit

--- accordion.js
+++ accordion.js
@@ -7,7 +7,9 @@ for(i=0;i<accordion.length;i++)
 {
     accordion[i].addEventListener('click',function(){
 
-        // this referes the Object that is currently calling the function
+        // 'this' referes to the Object that is currently calling the function which is in this case accordion
+
+        // The classList property returns the CSS classnames of an element and in this 'active' is the class name
         this.classList.toggle('active');
     })
 }
