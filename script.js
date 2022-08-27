1fd0c9bdc703cff4cf8fd67da16823db82fa36e9
gaurav-kumar17 <gk170106@gmail.com>
2022-08-27 21:23:14 +0530

    added javascript

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let flag=0;
+
+function controller(x)
+{
+    flag=flag+x;
+
+    //left
+    // flag=2+ (-1)=1;
+
+    //right
+    // flag=2+1=3;
+    slideshow(flag);
+}
+
+slideshow(flag);
+
+function slideshow(num)
+{
+    let slides=document.getElementsByClassName('slide');
+    
+    
+    if(num == slides.length)
+    {
+        num=0;
+        flag=0;
+    }
+    if(num < 0)
+    {
+        num=slides.length-1;
+        flag=slides.length-1;
+    }
+    for(let slide of slides)
+    {
+        slide.style.display="none";
+    }
+    slides[num].style.display="block";
+}
